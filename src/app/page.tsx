e993122e18acbf602908852ce2ae668f22c6efcf
Nawael <j.gunst@gmail.com>
2024-05-30 17:54:25 +0200

    add API_KEY add API_KEY to github secret

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
-import styles from '@/style/page.module.css';
 import getSkills from '@/api/skills';
 
 const Home = async () => {
   const skills = await getSkills();
 
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <code className={styles.code}>{skills && skills.map(({ label }: { label: string }) => <p>{label}</p>)}</code>
+    <main>
+      <div>
+        <code>{skills && skills.map(({ label }: { label: string }) => <p>{label}</p>)}</code>
       </div>
     </main>
   );
