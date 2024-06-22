c4d629aff85a4b48d816dc949cece9033a6677bd
Nawael <j.gunst@gmail.com>
2024-06-22 09:42:12 +0200

    release

--- src/components/skills/Skill.tsx
+++ src/components/skills/Skill.tsx
@@ -3,24 +3,27 @@ import splitArray from '@/utils/splitArray'
 // == TypeScript ==
 import type { Skill as SkillType } from '@/type'
 
-const Skill: React.FC<SkillType> = ({ name, data }) => (
-  <>
-    <h3>{name}</h3>
+const Skill: React.FC<SkillType> = ({ name, data }) => {
+  console.log(`%c splitArray(data) ->`, 'color:orange;font-weight:bold', splitArray(data))
+  return (
+    <>
+      <h3>{name}</h3>
 
-    <section>
-      {splitArray(data).map((items, key) => (
-        <ul key={key}>
-          {items.map(({ name, url }, key) => (
-            <li key={key}>
-              <a href={url} target="_blank" rel="noopener noreferrer">
-                {name}
-              </a>
-            </li>
-          ))}
-        </ul>
-      ))}
-    </section>
-  </>
-)
+      <section>
+        {splitArray(data).map((items, key) => (
+          <ul key={key}>
+            {items.map(({ name, url }, key) => (
+              <li key={key}>
+                <a href={url} target="_blank" rel="noopener noreferrer">
+                  {name}
+                </a>
+              </li>
+            ))}
+          </ul>
+        ))}
+      </section>
+    </>
+  )
+}
 
 export default Skill
