7611dc656e630a35765dfafb93ac1b22068e10af
Nawael <j.gunst@gmail.com>
2024-06-29 00:39:21 +0200

    oups

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
-import { FC } from 'react'
+import { FC, PropsWithChildren } from 'react'
 
 // == CSS ==
 import '@/app/main.css'
 
-const RootLayout: FC<React.PropsWithChildren> = ({ children }) => (
+const RootLayout: FC<PropsWithChildren> = ({ children }) => (
   <html lang="fr" className="font-display" suppressHydrationWarning>
     <body className="print:m-0 print:max-h-[29.7cm] print:max-w-[21cm] print:p-0">{children}</body>
   </html>
